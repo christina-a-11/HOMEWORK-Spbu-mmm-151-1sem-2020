da3598a9ba6251b263fce2f68520d4f13d851354
christina-a-11 <53395252+christina-a-11@users.noreply.github.com>
2020-12-16 22:23:07 +0300

    add homework star

--- 20.12.16-Homework-star/Task-9/Source.cpp
+++ 20.12.16-Homework-star/Task-9/Source.cpp
@@ -1,3 +1,4 @@
+/* с массивом
 #include <iostream>
 #include <vector>
 
@@ -26,5 +27,30 @@ int main()
 	ans = ans / (n - (double)1);
 	cout << sqrt(ans);
 	system("pause>nul");
+	return 0;
+}
+без массива*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double n = 0;
+	double sum = 0;
+	double sum_sq = 0;
+	double x = 0;
+	cin >> x;
+	while (x != 0)
+	{
+		n++;
+		sum += x;
+		sum_sq += x * x;
+		cin >> x;
+	}
+	cout << fixed << setprecision(11) << sqrt((sum_sq - ((sum*sum / n))) / (n - (double)1));
+	//system("pause>nul");
 	return 0;
 }
