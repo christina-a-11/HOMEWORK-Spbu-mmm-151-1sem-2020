c8d1a5dec7e0e8e89d9b2f470bb674b56b9d5404
christina-a-11 <53395252+christina-a-11@users.noreply.github.com>
2020-12-16 23:52:37 +0300

    add homework-6

--- 20.10.26-Homework-6/Task1/Source.cpp
+++ 20.10.26-Homework-6/Task1/Source.cpp
@@ -15,124 +15,120 @@ void printMenu()
 }
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	ArrayList are;
-	int choice = 0;
-	do
-	{
-		printMenu();
-		cin >> choice;
-		switch (choice)
+setlocale(LC_ALL, "Russian");
+ArrayList are;
+int choice = 0;
+do
+{
+printMenu();
+cin >> choice;
+switch (choice)
+{
+case 1:
+{
+	cout << "Введите добавляемый элемент" << endl;
+	int tmp = 0;
+	cin >> tmp;
+	are.add(tmp);
+}
+break;
+case 2:
+{
+	cout << are.toString() << endl;
+}
+break;
+case 3:
+{
+	bool flag = true;
+	for (int i = 0; i < are.length() / 2; i++)
 		{
-		case 1:
-		{
-			cout << "Введите добавляемый элемент" << endl;
-			int tmp = 0;
-			cin >> tmp;
-			are.add(tmp);
-
+			if (are.get(i) != are.get(are.length() - 1 - i))
+			 {
+				cout << "Массив не симметричен" << endl;
+				i += are.length();
+				flag = false;
+			 }
 		}
-		break;
-		case 2:
-		{
-			cout << are.toString() << endl;
-		}
-		break;
-		case 3:
-		{
-			bool flag = true;
+		if (flag == true)
+		 {
+			cout << "Массив симметричен" << endl;
+		 }
+}
+break;
+case 4:
+{
+	if (are.length() > 0)
+	 {
+		int n = 0;
+		cin >> n;
+		if (n > 0)
+		 {
+			n = are.length() - n % are.length();
+			for (int i = 0; i < n / 2; i++)
+			 {
+				are.swap(i, n - 1 - i);
+			 }
+			for (int i = 0; i < (are.length() - n) / 2; i++)
+			 {
+				are.swap(n + i, are.length() - 1 - i);
+			 }
 			for (int i = 0; i < are.length() / 2; i++)
-			{
-				if (are.get(i) != are.get(are.length() - 1 - i))
-				{
-					cout << "Массив не симметричен" << endl;
-					i += are.length();
-					flag = false;
+			 {
+				are.swap(i, are.length() - 1 - i);
+			 }
+		  }
+		 else  if (n < 0)
+		 {
+			n *= -1;
+			for (int i = 0; i < n / 2; i++)
+			 {
+				are.swap(i, n - 1 - i);
+			 }
+			for (int i = 0; i < (are.length() - n) / 2; i++)
+			 {
+				are.swap(n + i, are.length() - 1 - i);
+			 }
+			for (int i = 0; i < are.length() / 2; i++)
+			 {
+			        are.swap(i, are.length() - 1 - i);
+			 }
 
-				}
-			}
-			if (flag == true)
-			{
-				cout << "Массив симметричен" << endl;
-			}
 		}
-		break;
-		case 4:
+	}
+}
+break;
+case 5:
+{
+   bool flag = true;
+
+   for (int i = 0; i < are.length() - 1; i++)
+	{
+	   int temp = 0;
+	   temp = are.get(i); 
+	   are.remove(i); // delete element i
+	   for (int j = 0; j < are.length() / 2; j++)
 		{
-			if (are.length() > 0)
-			{
-				int n = 0;
-				cin >> n;
-				if (n > 0)
-				{
-					n = are.length() - n % are.length();
-					for (int i = 0; i < n / 2; i++)
-					{
-						are.swap(i, n - 1 - i);
-					}
-					for (int i = 0; i < (are.length() - n) / 2; i++)
-					{
-						are.swap(n + i, are.length() - 1 - i);
-					}
-					for (int i = 0; i < are.length() / 2; i++)
-					{
-						are.swap(i, are.length() - 1 - i);
-					}
-				}
-				else  if (n < 0)
-				{
-					n *= -1;
-					for (int i = 0; i < n / 2; i++)
-					{
-						are.swap(i, n - 1 - i);
-					}
-					for (int i = 0; i < (are.length() - n) / 2; i++)
-					{
-						are.swap(n + i, are.length() - 1 - i);
-					}
-					for (int i = 0; i < are.length() / 2; i++)
-					{
-						are.swap(i, are.length() - 1 - i);
-					}
-
-				}
-			}
-		}
+		  if (are.get(j) != are.get(are.length() - 1 - j))
+		    {
+			flag = false;
 			break;
-		case 5:
-		{
-			bool flag = true;
-
-			for (int i = 0; i < are.length() - 1; i++)
-			{
-				int temp = 0;
-				temp = are.get(i); 
-				are.remove(i); // delete element i
-
-				for (int j = 0; j < are.length() / 2; j++)
-				{
-					if (are.get(j) != are.get(are.length() - 1 - j))
-					{
-						flag = false;
-						break;
-					}
-				}
-
-				if (flag == true)
-				{
-					cout << "Массив симметричен, если удалить элемент" << endl;
-					i += are.length() + 1;
-				}
-				are.set(i, temp); // add element i
-			}
-			if (flag == false)
-			{
-				cout << "Массив не может стать симметричным" << endl;
-			}
-		}
-		break;
-		system("pause>nul");
-		}
-	} while (choice != 0);
+		     }
+	         }
+ if (flag == true)
+ {
+   cout << "Массив симметричен, если удалить элемент" << endl;
+   i += are.length() + 1;
+  }
+ are.set(i, temp); // add element i
+ }
+ if (flag == false)
+ {
+  cout << "Массив не может стать симметричным" << endl;
+ }
+}
+break;
+system("pause>nul");
+}
+} while (choice != 0);
 	return 0;
 }
