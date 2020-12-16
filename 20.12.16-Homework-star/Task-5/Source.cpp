537e47d8851394bd06d00d169090c0708de70499
christina-a-11 <53395252+christina-a-11@users.noreply.github.com>
2020-12-16 19:50:51 +0300

    add homework star

---
+++ 20.12.16-Homework-star/Task-5/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k = 1, m = 1, n = 1;
+	cin >> k >> m >> n;
+	int t = 0;
+	t = n / k * m * 2;
+	if ((n % k) > 0) t += m;
+	cout << t;
+	system("pause>nul");
+	return 0;
+}
