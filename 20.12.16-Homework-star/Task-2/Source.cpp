3c4e4fd25018e26eef1e5d059635f4a33ae8a0af
christina-a-11 <53395252+christina-a-11@users.noreply.github.com>
2020-12-17 14:08:01 +0300

    add homework star

--- 20.12.16-Homework-star/Task-2/Source.cpp
+++ 20.12.16-Homework-star/Task-2/Source.cpp
@@ -6,8 +6,9 @@ int main()
 {
 	int n = 1, m = 1;
 	cin >> n >> m;
-	double k = (double)max(n, m) / min(n, m);
+	double k = 0;
+	k = (n / m > 0 ? (double)n / m : (double)m / n);
 	cout << (k == (int)k ? 1 : 5);
-	system("pause>nul");
+	//system("pause>nul");
 	return 0;
 }
