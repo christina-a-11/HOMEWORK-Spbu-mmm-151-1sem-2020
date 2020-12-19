9ca1e893b2b3fbd50518e05f95413822695b8dd1
christina-a-11 <53395252+christina-a-11@users.noreply.github.com>
2020-12-19 09:35:19 +0300

    add homework-10 filling array

--- 20.12.18-Homework-10/Task1/Source.cpp
+++ 20.12.18-Homework-10/Task1/Source.cpp
@@ -80,21 +80,21 @@ void Arr5(int n, int**& a)
 	if (n % 2 != 0) {
 		a[(n / 2)][(n / 2)] = (n * n);
 	}
-	for (int i = 0; i < (n / 2); i++) {
-		for (int j = i; j < (n - i); j++) {
+	for (int i = 0; i < n / 2; i++) {
+		for (int j = i; j < n - i; j++) {
 			a[i][j] = m;
 			m++;
 		}
-		for (int j = 1; j < (n - i - i); j++) {
-			a[(j + i)][(n - i) - 1] = m;
+		for (int j = 1; j < n - i - i; j++) {
+			a[(j + i)][n - i - 1] = m;
 			m++;
 		}
-		for (int j = (n - 2) - i; j >= i; j--)
+		for (int j = n - 2 - i; j >= i; j--)
 		{
-			a[(n - i) - 1][(j)] = m;
+			a[n - i - 1][(j)] = m;
 			m++;
 		}
-		for (int j = ((n - i) - 2); j > i; j--) {
+		for (int j = (n - i - 2); j > i; j--) {
 			a[j][i] = m;
 			m++;
 		}
